9f9e0df45f6c12cc018f1135c764f86a7037e10d
Yoonstar <107985535+yoonstar1996@users.noreply.github.com>
2025-04-15 10:47:51 +0900

    공통 Label, Input 컴포넌트 생성 (#13)
    * feat: 공통 Label 컴포넌트 생성성
    
    * feat: 공통 Input 컴포넌트 생성
    
    * chore: 추가 폴더 생성
    
    * refactor: 공통 컴포넌트 "use client" 작성

---
+++ app/components/shared/input/Input.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { cn } from '../root-header/lib/cn';
+
+type InputProps = {
+  className?: string;
+  placeholder?: string;
+  error?: boolean;
+  id?: string;
+};
+
+export default function Input({ className, placeholder, error, id }: InputProps) {
+  return (
+    <input
+      className={cn(
+        'placeholder-gray-6 border px-4 py-3 text-sm',
+        error ? 'border-error' : 'border-gray-4',
+        className
+      )}
+      type="text"
+      placeholder={placeholder}
+      id={id}
+    />
+  );
+}
---
+++ app/components/shared/label/Label.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+type LabelProps = {
+  label: string;
+  htmlFor?: string;
+};
+
+export default function Label({ htmlFor, label }: LabelProps) {
+  return (
+    <label className="text-gray-7 text-sm" htmlFor={htmlFor}>
+      {label}
+    </label>
+  );
+}
