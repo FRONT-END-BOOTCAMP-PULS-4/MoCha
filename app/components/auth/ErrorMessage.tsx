9241de1525bdd8a200516980a865ae7e26680930
Yoonstar <yagobo1110@naver.com>
2025-04-16 10:31:48 +0900

    feat: 에러 메세지 컴포넌트 삭제

--- app/components/auth/ErrorMessage.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { ReactNode } from 'react';
-
-export default function ErrorMessage({ children }: { children: ReactNode }) {
-  return <div className="text-error mt-1 min-h-[16px] text-xs">{children}</div>;
-}
