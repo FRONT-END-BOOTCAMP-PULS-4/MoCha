e3f5227b1cc213ee70736d6ad1b3809984045c07
imi21123 <imi21123@naver.com>
2025-04-17 12:22:29 +0900

    style: z-index 조정

--- app/components/main/FloatingButton.tsx
+++ app/components/main/FloatingButton.tsx
@@ -4,7 +4,7 @@ import { Plus } from 'lucide-react';
 export default function FloatingButton({ onClick }: FloatingButtonProps) {
   return (
     <button
-      className="bg-main hover:bg-main-hover fixed right-4 bottom-4 z-50 flex h-14 w-14 items-center justify-center rounded-full focus:outline-none"
+      className="bg-main hover:bg-main-hover fixed right-4 bottom-4 z-30 flex h-14 w-14 items-center justify-center rounded-full focus:outline-none"
       onClick={onClick}
     >
       <Plus stroke="white" />
